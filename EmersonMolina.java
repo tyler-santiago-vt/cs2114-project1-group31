0112600532958abee55f05cf488a5a4c6167a02e
tyler-santiago-vt <tylermsantiago@vt.edu>
2026-09-15 14:31:06 -0400

    Set up as java project and added class files

--- EmersonMolina.java
+++
@@ -1,5 +0,0 @@
-public class Emerson Molina {
-    public static void main(String[] args) {
-        System.out.println("Your Name is on the team.");
-    }
-}
---
+++ src/trivia/Main.java
@@ -0,0 +1,16 @@
+package trivia;
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
