22caf017b9bac0a2325120b23cff2dbe038afe02
poornab26 <135745242+pkb34@users.noreply.github.com>
2026-09-15 15:29:33 -0400

    updated ScoreTracker and Main classes

--- src/trivia/Main.java
+++ src/trivia/Main.java
@@ -1,16 +1,56 @@
 package trivia;
+
+import java.util.Scanner;
+
+/**
+ * // -------------------------------------------------------------------------
+ * /** Write a one-sentence summary of your class here. Follow it with
+ * additional details about its purpose, what abstraction it represents, and how
+ * to use it.
+ * 
+ * @author poornabhat
+ * @version Sep 15, 2026
+ */
 public class Main
 {
-
+    /**
+     * 
+     */
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
+        ScoreTracker score = new ScoreTracker();
+        Question questions;
+        GameController game = new GameController();
+        QuestionBank questionBank;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print(
+            "Please pick a catergory: Music, History, People, or Geography");
+
+        String userInput = input.nextLine();
+
+        if (userInput.equalsIgnoreCase("Music"))
+        {
+
+        }
+        else if (userInput.equalsIgnoreCase("History"))
+        {
+
+        }
+        else if (userInput.equalsIgnoreCase("People"))
+        {
+
+        }
+        else if (userInput.equalsIgnoreCase("Geography"))
+        {
+
+        }
+        else
+        {
+            System.out.print("Not a valid catergory. Try Again!");
+            userInput = input.nextLine();
+        }
 
     }
-    //~ Fields ................................................................
-
-    //~ Constructors ..........................................................
-
-    //~Public  Methods ........................................................
 
 }
