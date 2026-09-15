86cb92c7b2f981edd66f7bb3e74c3d262f39cc68
tyler-santiago-vt <tylermsantiago@vt.edu>
2026-09-15 15:11:56 -0400

    Implement GameController method endGame()

--- src/trivia/GameController.java
+++ src/trivia/GameController.java
@@ -3,11 +3,22 @@ public class GameController
 {
     //~ Fields ................................................................
 
+    private ScoreTracker score;
+    
     //~ Constructors ..........................................................
 
     //~Public  Methods ........................................................
 
     public void endGame(boolean win)
+    {
+        String message;
+        if (win == true) {
+            message = "Correct! You've answered all questions correctly! Score: ";
+        } else {
+            message = "Incorrect, Game Over! Score: ";
+        }
+        
+        System.out.println(message + score.getScore());
     }
     
     public void endCategory()
